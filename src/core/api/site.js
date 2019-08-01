9a15fa4554045e83dc6976af49f4df46b24970b2
Dmitri Don <fotkin@gmail.com>
2019-08-01 21:25:03 +0300

    Change site init info url

--- src/core/api/site.js
+++ src/core/api/site.js
@@ -2,5 +2,5 @@ import { get } from './../lib/net';
 
 
 export function getInit(apiUrl) {
-    return get(`${apiUrl}/wp-json/svelte-wp/v1/init`);
+    return get(`${apiUrl}/wp-json/svelte-wp/v1/site/init`);
 }
