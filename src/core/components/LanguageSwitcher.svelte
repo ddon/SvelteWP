74d25e0f0162fb77b841396f0c29b482cca2ce22
Dmitri Don <fotkin@gmail.com>
2019-08-17 02:11:10 +0300

    Add js to language switcher

--- src/core/components/LanguageSwitcher.svelte
+++ src/core/components/LanguageSwitcher.svelte
@@ -1,4 +1,5 @@
 <script>
+import { fly } from 'svelte/transition';
 import { RouterLink } from '@svel/router';
 
 export let languages = null;
@@ -6,6 +7,7 @@ export let currentLanguage = '';
 export let dropdown = false;
 
 
+let isVisibleMenu = false;
 let currentLang = null;
 
 
@@ -28,6 +30,21 @@ function getCurrentLang() {
 }
 
 
+function toggleMenu(langSlug) {
+    isVisibleMenu = !isVisibleMenu;
+
+    if (langSlug) {
+        for (let i = 0; i < languages.length; i += 1) {
+            const lang = languages[i];
+            if (lang.slug === langSlug) {
+                currentLang = lang;
+                break;
+            }
+        }
+    }
+}
+
+
 $: {
     if (languages.length > 0) {
         currentLang = getCurrentLang();
@@ -38,57 +55,56 @@ $: {
 <style>
 .languages {
     display: flex;
+    align-items: center;
 }
 
 .languages .language-link {
-    padding: 10px 15px 10px 15px;
+    display: flex;
+}
+
+.languages .language-link:hover {
+    background-color: #0178a9;
 }
 
 .languages-dropdown {
     flex: 1 0 auto;
-
     display: flex;
     justify-content: flex-end;
 
     position: relative;
 
     transition: 200ms;
-    transition-delay: 200ms;
 }
 
 .languages-dropdown .language-link {
-    min-width: 65px;
+    min-width: 91px;
 
     display: flex;
     align-items: center;
-    padding: 10px 15px 10px 15px;
 
     transition: 200ms;
-    transition-delay: 200ms;
 }
 
 .languages-dropdown .languages-dropdown-menu {
     position: absolute;
     top: 0;
     right: 0;
-
-    background-color: #dddddd;
-
-    visibility: hidden;
-    opacity: 0;
     z-index: 100;
 
+    background-color: #0178a9;
+
     transition: 200ms;
-    transition-delay: 200ms;
 }
 
-.languages-dropdown:hover .language-link {
+.languages-dropdown .language-link:hover {
     background-color: #0178a9;
 }
 
-.languages-dropdown:hover .languages-dropdown-menu {
-    visibility: visible;
-    opacity: 1;
+.languages-dropdown .languages-dropdown-menu .language-link {
+}
+
+.languages-dropdown .languages-dropdown-menu .language-link:hover {
+    background-color: #0190ca;
 }
 </style>
 
@@ -97,27 +113,29 @@ $: {
     {#if dropdown}
         <nav class='languages-dropdown'>
             {#if currentLang}
-                <div class='language-link'>
-                    <RouterLink to={currentLang.link} style='color: #ffffff;'>
+                <div class='language-link' on:click='{toggleMenu}'>
+                    <RouterLink to={currentLang.link} style='color: #ffffff; padding: 16px 15px 16px 15px; width: 100%;'>
                         {currentLang.name}
                     </RouterLink>
                 </div>
             {/if}
-            <div class='languages-dropdown-menu'>
-                {#each languages as lang}
-                    <div class='language-link'>
-                        <RouterLink to={lang.link} style='color: #ffffff;'>
-                            {lang.name}
-                        </RouterLink>
-                    </div>
-                {/each}
-            </div>
+            {#if isVisibleMenu}
+                <div class='languages-dropdown-menu' transition:fly='{{duration: 300, x: 0}}' on:mouseleave='{toggleMenu}'>
+                    {#each languages as lang}
+                        <div class='language-link' on:click={() => { toggleMenu(lang.slug); }}>
+                            <RouterLink to={lang.link} style='color: #ffffff; padding: 16px 15px 16px 15px; width: 100%;'>
+                                {lang.name}
+                            </RouterLink>
+                        </div>
+                    {/each}
+                </div>
+            {/if}
         </nav>
     {:else}
         <nav class='languages'>
             {#each languages as lang}
                 <div class='language-link'>
-                    <RouterLink to={lang.link} style='color: #ffffff;'>
+                    <RouterLink to={lang.link} style='color: #ffffff; padding: 16px 15px 16px 15px;'>
                         {lang.name}
                     </RouterLink>
                 </div>
